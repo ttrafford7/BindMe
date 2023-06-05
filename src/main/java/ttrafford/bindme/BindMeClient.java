a9e738687f2577cdcb175c6084e8bc6521765448
ttrafford <maks.kachorak@gmail.com>
2023-06-05 23:06:45 +0300

    original post

---
+++ src/main/java/ttrafford/bindme/BindMeClient.java
@@ -0,0 +1,10 @@
+package ttrafford.bindme;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class BindMeClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/ttrafford/bindme/BindMeDataGenerator.java
@@ -0,0 +1,11 @@
+package ttrafford.bindme;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class BindMeDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
